82df5b218e3f57bb32c638613c66989b626c26b6
BlackBrocolli <201111011@mhs.stiki.ac.id>
2023-05-26 13:45:40 +0700

    [FEAT] Menambahkan fitur edit dan delete produk

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,7 @@ use App\Models\Product;
 use App\Models\ViewProduct;
 use App\Models\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Intervention\Image\Facades\Image;
 
 class ProductController extends Controller
@@ -54,7 +55,7 @@ class ProductController extends Controller
         $this->validate($request, [
             'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'name' => 'required|string|max:100',
-            'description' => 'required|string|max:255',
+            'description' => 'required|string',
             'category' => 'required',
             'price' => 'required|numeric',
         ]);
@@ -114,7 +115,12 @@ class ProductController extends Controller
      */
     public function edit(Product $product)
     {
-        //
+        $categories = Category::orderBy('name')->get();
+        $navitem = 'produk';
+        $navitemchild = 'daftar-produk';
+
+        // return view
+        return view('products.edit', compact('product', 'categories', 'navitem', 'navitemchild'));
     }
 
     /**
@@ -126,7 +132,61 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        //validate form
+        $this->validate($request, [
+            'image' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'name' => 'required|string|max:100',
+            'description' => 'required|string',
+            'category' => 'required',
+            'price' => 'required|numeric',
+        ]);
+
+        //check if image is uploaded
+        if ($request->hasFile('image')) {
+
+            //upload image
+            $image = $request->file('image');
+            $imageName = $image->hashName();
+            // resize image agar serasi
+            $imagePath = 'assets-landing/img/portfolio/' . $imageName;
+            Image::make($image)
+                ->resize(800, 600)
+                ->save(public_path($imagePath));
+
+            //delete old image
+            $oldImagePath = public_path('assets-landing/img/portfolio/' . $product->image);
+            File::delete($oldImagePath);
+
+            //update product with new image
+            $product->update([
+                'image' => $imageName,
+                'name' => $request->name,
+                'category_id' => $request->category,
+                'description' => $request->description,
+                'price' => $request->price,
+                // status default waiting
+                'status' => 'waiting',
+                // created_by sementara 1 dulu
+                // karena belum ada user login
+                'created_by' => 1,
+            ]);
+        } else {
+            //update user without avatar
+            $product->update([
+                'name' => $request->name,
+                'category_id' => $request->category,
+                'description' => $request->description,
+                'price' => $request->price,
+                // status default waiting
+                'status' => 'waiting',
+                // created_by sementara 1 dulu
+                // karena belum ada user login
+                'created_by' => 1,
+            ]);
+        }
+
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Produk Berhasil Diubah!']);
     }
 
     /**
@@ -137,6 +197,13 @@ class ProductController extends Controller
      */
     public function destroy(Product $product)
     {
-        //
+        //delete image
+        $oldImagePath = public_path('assets-landing/img/portfolio/' . $product->image);
+        File::delete($oldImagePath);
+
+        //delete product
+        $product->delete();
+        //redirect to index
+        return redirect()->route('products.index')->with(['success' => 'Data Produk Berhasil Dihapus!']);
     }
 }
